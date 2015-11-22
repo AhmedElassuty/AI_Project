0f3e404e65f9c8a021318b89aca3db7747ea44c2
Mohamed Diaa <mohamed.diaa27@gmail.com>
2015-11-23 01:37:32 +0200

    fixing bugs

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # AI_Project_2
+
+Libraries used for parsring : Whittle
+ Whittle: A little LALR(1) Parser in Pure Ruby — Not a Generator
--- main.rb
+++ main.rb
@@ -7,13 +7,16 @@ parser = Parser.new
 
 inputs = [ "∃x[P(x)∧∀x[Q(x)⟹¬P(x)]]", "∀x[P (x) ⟺ (Q(x) ∧ ∃y[Q(y) ∧ R(y, x)])]", "B(a)", "¬P (x) ∧ Q(z)" ]
 
-inputs.each do |i|
-	p  "Parsing   >>>>>>>>>>   " + i 
-	p "-"*200
-  p parser.parse_sentence(i)
-  p "+"*200
-end 
+# inputs.each do |i|
+# 	p  "Parsing   >>>>>>>>>>   " + i 
+# 	p "-"*200
+#   p parser.parse_sentence(i)
+#   p "+"*200
+# end 
 
+
+input = "P(x) ∧ Q(x) ∨ T(x) ⟹ Z(x)"
+p parser.parse_sentence(input)
 p "Thank You ;)"
 
 
