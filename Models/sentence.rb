79b58555534c5bb91be9d0a7f3c8e5a7c9e307dd
AhmedElassuty <ahmed.elassuty214@gmail.com>
2015-11-24 15:58:49 +0200

    resolve bi_implication && implication

--- Models/sentence.rb
+++ Models/sentence.rb
@@ -2,6 +2,7 @@ class Sentence
 end
 
 class Predicate < Sentence
+  attr_accessor :name, :terms
   @name = nil
   @terms = []
 
@@ -13,17 +14,23 @@ class Predicate < Sentence
   def pretty_print
     @name + LEFT_PARENTHESIS_SYMBOL + @terms.map { |t| t.pretty_print}.join(",") + RIGHT_PARENTHESIS_SYMBOL
   end
+
+  def step_1
+    self.clone
+  end
+  alias_method :step_2, :step_1
+
 end
 
-class ConnectiveSentence < Sentence  
+class ConnectiveSentence < Sentence
+  attr_accessor :sentence1, :sentence2
+
   #
   # A super class for Sentences that have 2 sentences as input
   # Params:
   #      @sentence1: The first sentence
   #      @sentence2: The Second sentence
   #
-  @sentence1 = nil
-  @sentence2 = nil
 
   def initialize(sentence1, sentence2)
     @sentence1 = sentence1
@@ -33,6 +40,20 @@ class ConnectiveSentence < Sentence
   def print(symbol)
     LEFT_PARENTHESIS_SYMBOL + @sentence1.pretty_print\
       + symbol + @sentence2.pretty_print + RIGHT_PARENTHESIS_SYMBOL
+  end
+
+  def step_1
+    output = self.clone
+    output.sentence1 = @sentence1.step_1
+    output.sentence2 = @sentence2.step_1
+    output
+  end
+
+  def step_2
+    output = self.clone
+    output.sentence1 = @sentence1.step_2
+    output.sentence2 = @sentence2.step_2
+    output
   end
 
 end
@@ -59,31 +80,15 @@ class Or < ConnectiveSentence
 
 end
 
-class Implication < ConnectiveSentence
-  #
-  # Class responsible for holding the ⟹ of 2 sentences
-  #
-
-  # P ⟹ Q == ¬P ∨ Q
-  def resolve
-    Or.new(Not.new(@sentence1), @sentence2)
-  end
-
-  def pretty_print
-    print IMPLICATION_SYMBOL + "  "
-  end
-
-end
-
 class BiConditional < ConnectiveSentence
   #
   # Class responsible for holding the ⟺ of 2 sentences
   #
 
   # P ⟺ Q == (P ⟹ Q) ∧ (Q ⟹ P)
-  def resolve
-    And.new(Implication.new(@sentence1.clone, @sentence2.clone),
-            Implication.new(@sentence2.clone, @sentence1.clone))
+  def step_1
+    And.new(Implication.new(@sentence1.step_1, @sentence2.step_1),
+            Implication.new(@sentence2.step_1, @sentence1.step_1))
   end
 
   def pretty_print
@@ -92,14 +97,29 @@ class BiConditional < ConnectiveSentence
 
 end
 
+class Implication < ConnectiveSentence
+  #
+  # Class responsible for holding the ⟹ of 2 sentences
+  #
+
+  # P ⟹ Q == ¬P ∨ Q
+  def step_2
+    Or.new(Not.new(@sentence1.step_2), @sentence2.step_2)
+  end
+
+  def pretty_print
+    print IMPLICATION_SYMBOL + "  "
+  end
+
+end
+
 class Not < Sentence
+  attr_accessor :sentence
   #
   # A Class responsible for (¬) of a Sentence
   # Params:
   #      @sentence: A sentence
   #
-
-  @sentence = nil
   
   def initialize(sentence)
     @sentence = sentence
@@ -109,21 +129,44 @@ class Not < Sentence
     NOT_SYMBOL + LEFT_PARENTHESIS_SYMBOL +  @sentence.pretty_print + RIGHT_PARENTHESIS_SYMBOL
   end
 
+  def step_1
+    output = self.clone
+    output.sentence = @sentence.step_1
+    output
+  end
+
+  def step_2
+    output = self.clone
+    output.sentence = @sentence.step_2
+    output
+  end
+
 end
 
 class QuantifierSentence  < Sentence
+  attr_accessor :sentence, :variable
+
   #
   # Params:
   #      @sentence: sentence which The Quantifier defines
   #      @variable: varibale which The Quantifier defines
   #
 
-  @sentence = nil
-  @variable = nil
-  
   def initialize(variable, sentence)
     @variable = variable #String
     @sentence = sentence
+  end
+
+  def step_1
+    output = self.clone
+    output.sentence = @sentence.step_1
+    output
+  end
+
+  def step_2
+    output = self.clone
+    output.sentence = @sentence.step_2
+    output
   end
 
 end
--- Modules/CNF.rb
+++ Modules/CNF.rb
@@ -3,42 +3,85 @@ module CNF
   @@stepTrack = false
 
   def self.execute(sentence, stepTrack= false)
-    stepTrack = stepTrack
-    step1 sentence
+    puts "-----------------CNF Transformation---------------------"
+    puts "FOL sentence: \n"\
+      + sentence.pretty_print
+
+    @@stepTrack = stepTrack
+    resolve_bi_conditional sentence
   end
 
-  # resolving bi-conditional implication
-  def self.step1(sentence)
-
-    sentence.pretty_print
+  # Resolving bi-conditional implication
+  def self.resolve_bi_conditional(sentence)
+    output = sentence.step_1
+    step_print("Step 1 (resolving bi-conditional implication)", output)
+    resolve_implication output
   end
 
-  # resolving implication
-  def self.step2(sentence)
-    
+  # Resolving implication
+  def self.resolve_implication(sentence)
+    output = sentence.step_2
+    step_print("Step 2 (resolving implication)", output)
+    move_negation_inward output
   end
 
-  def self.step3(sentence)
-    
+  # Moving not operator inward
+  def self.move_negation_inward(sentence)
+    output = sentence.step_3
+    step_print("Step 3 (moving ¬ operator inward)", output)
   end
 
+  # Renaming quantifier variables
   def self.step4(sentence)
     
-  end
-
-  def self.step5(sentence)
     
   end
 
+  # Skolemize
+  def self.step5(sentence)
+    
+    
+  end
+
+  # Discard forAll quantifier
   def self.step6(sentence)
     
   end
 
+  # Conjunctions of disjunctions
   def self.step7(sentence)
+    
+    puts sentence.pretty_print if @@stepTrack
+  end
+
+  # Flatten nested conjunctions and disjunctions
+  def self.step8(sentence)
+    
     
   end
 
-  def self.step8(sentence)
+  # remove OR symbols
+  def self.step9(sentence)
+    
     
   end
+
+  # remove AND symbols
+  def self.step10(sentence)
+    
+    
+  end
+
+  # remove rename CNF clauses
+  def self.step9(sentence)
+    
+    
+  end
+
+  #  Helper methods
+  def self.step_print(msg, sentence)
+    puts msg + ":\n"\
+      + sentence.pretty_print if @@stepTrack
+  end
+
 end
