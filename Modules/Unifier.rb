6b32a7f99020245e87634b22d47d883ebd12f6cb
Kareem Diaa <kareem.diaa@gmail.com>
2015-11-28 17:56:55 +0200

    fixing unification

--- Modules/Unifier.rb
+++ Modules/Unifier.rb
@@ -3,35 +3,43 @@ module Unifier
 	def self.execute(atom1, atom2, stepTrack= false)
     puts "----------------- Unificication ---------------------"
     @@stepTrack = stepTrack
-    	# unless atom1.instance_of?(atom2.class)
-    	# 	return p atom1.class.name + " is not a " + atom2.class.name
-    	# end
     self.unify(atom1, atom2, {})
   end
 
   def self.unify(atom1, atom2, hash_map)
 
-  	if hash_map.nil? 
+  	if hash_map.nil?
+  		## Faild to Unify Previous
   		return nil
   	elsif atom1.equals?(atom2)
+  		## Identical Atoms
   		return hash_map
   	elsif atom1.instance_of?(VariableTerm)
+  		## Variable and Atom
   		return self.unify_variable(atom1, atom2, hash_map)
   	elsif atom2.instance_of?(VariableTerm)
+  		## Atom and Variable
   		return self.unify_variable(atom2, atom1, hash_map)
   	elsif (atom1.instance_of?(Predicate) and atom2.instance_of?(Predicate)) or (atom1.instance_of?(FunctionTerm) and atom2.instance_of?(FunctionTerm))
+  		## Predicate or Function Sentence
   		return self.unify_list_of_terms(atom1.terms, atom2.terms, self.unify_name(atom1, atom2, hash_map))
   	elsif (atom1.instance_of?(ForAll) and atom2.instance_of?(ForAll)) or (atom1.instance_of?(ThereExists) and atom2.instance_of?(ThereExists))
+  		## Quantifier Sentence
   		return self.unify(atom1.sentence, atom2.sentence, self.unify_variable(atom1.variable, atom1.variable, hash_map))
   	elsif (atom1.instance_of?(Not) and atom2.instance_of?(Not))
+  		## Not Sentence
   		return self.unify(atom1.sentence, atom2.sentence, hash_map)
+  	elsif (atom1.instance_of?(And) and atom2.instance_of?(And)) or (atom1.instance_of?(Or) and atom2.instance_of?(Or)) or (atom1.instance_of?(BiConditional) and atom2.instance_of?(BiConditional)) or (atom1.instance_of?(Implication) and atom2.instance_of?(Implication))
+  		## Connective Sentence
+  		return self.unify(atom1.sentence2, atom2.sentence2, self.unify(atom1.sentence1, atom2.sentence1(hash_map)))
   	else
+  		## Faild to Unify
   		return nil
   	end
   end
 
   def self.unify_name(atom1, atom2, hash_map)
-  	if (theta == null) 
+  	if hash_map.nil?
 			return nil
 		elsif atom1.name == atom2.name
 			return hash_map
